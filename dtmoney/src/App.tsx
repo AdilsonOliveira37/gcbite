de0b1f5ea30c399e7b82b98515b86f28a8e72c97
Adilson Oliveira <adilsonreisoliveira@hotmail.com>
2022-06-29 09:38:05 -0300

    module-02: dtmoney

---
+++ dtmoney/src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './hooks/useTransactions';
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionOpen(false);
+  }
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
+    </ TransactionsProvider>
+  );
+}
